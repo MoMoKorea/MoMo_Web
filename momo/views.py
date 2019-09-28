03d23d867d8ef88e886bf6d0226ec75eb2835c82
Kenny <yukiadada@gmail.com>
2019-09-28 19:48:26 +0900

    home style부분 추가 및 class 이름변경

--- momo/views.py
+++ momo/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    # return render(request, 'account/signup.html')
+    # return render(request, 'account/test.html')
     return render(request, 'home.html')
