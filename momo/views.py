d28de1aaef07e4b11c2032ee201f8f3062796e5d
Kenny <yukiadada@gmail.com>
2019-10-06 19:07:53 +0900

    error 모아놓은페이지

--- momo/views.py
+++ momo/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    return render(request, 'errors.html')
     # return render(request, 'account/verification_sent.html')
     # return render(request, 'account/password_reset.html')
     # return render(request, 'account/password_change.html')
     # return render(request, 'account/login.html')
-    return render(request, 'account/signup.html')
+    # return render(request, 'account/signup.html')
     # return render(request, 'account/test.html')
     # return render(request, 'home.html')
