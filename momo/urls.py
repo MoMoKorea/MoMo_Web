e798af0ca22fc070a7e4108d2aca726d58ed25af
Joe <esj1020@gmail.com>
2021-06-19 12:54:01 +0900

    fix: class-based view 변경 (urls.py)

--- momo/urls.py
+++ momo/urls.py
@@ -5,7 +5,8 @@ from job.urls import views as jobView
 
 
 urlpatterns = [
-    path('', jobView.get_list),
+    # path('', jobView.get_list),
+    path('', jobView.get_list.as_view()),
     path('home/', momoView.home),
     path('test/', momoView.test_current_datetime), # test
     path('test_class/', momoView.cur_time.as_view()), # test
